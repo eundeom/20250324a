ce5068756184a5734ece16c8089f9b1d7e43b13b
@eundeom <36tfuu@gmail.com>
2025-04-09 10:29:39 +0900

    update test file

--- step07_cicd/src/test/java/edu/fisa/ce/Step07CicdApplicationTests.java
+++ step07_cicd/src/test/java/edu/fisa/ce/Step07CicdApplicationTests.java
@@ -2,6 +2,7 @@ package edu.fisa.ce;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class Step07CicdApplicationTests {
